676e20e3d25178d6b8bfe2ab5b090d523e940aca
Mansom333 <kelly.zane@icloud.com>
2025-05-30 14:09:24 +1000

    Add files via upload

---
+++ data/projects.js
@@ -0,0 +1,65 @@
+window.projectsData = [
+    {
+        "title": "Market Product",
+        "image": "market-product.jpg",
+        "description": "Advanced agent based model simulating user adoption rate of market product with real time optimisation algorithms.",
+        "link": "https://cloud.anylogic.com/model/9a1fb851-7249-40aa-81d6-46b2dd39a905"
+    },
+    {
+        "title": "SEIR",
+        "image": "SEIR2.jpg", 
+        "description": "System dynamics simulation of infectious disease epidemiology. Applied a variation study to consider all possible outcomes for a given range",
+        "link": "https://cloud.anylogic.com/model/2918b2a3-c0cc-4748-8fe3-cea48d4f39af"
+    },
+    {
+        "title": "Workshop",
+        "image": "workshop.png",
+        "description": "Real time digital twin of workshop CNC facility with predictive algorithms and quality control systems.", 
+        "link": "https://cloud.anylogic.com/model/31a79e50-afe3-40cd-888c-c26bbd2c14f4"
+    }
+,
+    {
+        "title": "Airport",
+        "image": "airport.jpg", 
+        "description": "Simulation of airport, analysis of required working capacity and task duration optimisation.",
+        "link": "https://cloud.anylogic.com/model/07b0c177-abcc-48e1-9f67-eade10701087"
+    }
+,
+    {
+        "title": "Bank ATM Utilisation",
+        "image": "BankATM.jpg", 
+        "description": "Simulating the usage of a bank ATM to relieve demand on bank tellers and streamline customer service, minimising wait time.",
+        "link": "https://cloud.anylogic.com/model/0ba3aa1b-8344-41d2-af33-059c4899f0d8"
+    }
+
+,
+    {
+        "title": "Bank ATM Utilisation 2",
+        "image": "BankATM2.jpg", 
+        "description": "Variation incorporating additional factors such as user decision making to determine ATM usage compared to bank tellers, when additional support is needed.",
+        "link": "https://cloud.anylogic.com/model/da62a5d9-f4dc-4e53-8ff1-b874e34ff91b"
+    }
+,
+    {
+        "title": "Hospital Foot Traffic",
+        "image": "Hospital.jpg", 
+        "description": "Re-created emergency clinic floor plan to simulate foot traffic and service efficiency.",
+        "link": "https://cloud.anylogic.com/model/91a6096a-d47b-4d5d-b575-4b0188ce516c"
+    }
+
+,
+    {
+        "title": "Subway Entrance",
+        "image": "SubwayEntrance.jpg", 
+        "description": "Determining optimal placement of self service ticket machines based on pedestrian density in subway entrance.",
+        "link": "https://cloud.anylogic.com/model/e169b87e-ec96-43da-ac07-dd07a94bab43"
+    }
+
+,
+    {
+        "title": "Predator Prey (Foxes and Rabbits)",
+        "image": "Predator Prey.jpg", 
+        "description": "System dynamics relationship of a predator prey relationship, of Foxes and Rabbits.",
+        "link": "https://cloud.anylogic.com/model/1d5a647d-0fee-484b-96e7-023e835d82be"
+    }
+];
